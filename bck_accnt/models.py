f529fa2e8d82f58abc2aae6e15a18afc3c2073d7
diderot <dideroteffects@gmail.com>
2022-12-24 16:44:38 +0900

    temp2: migrations & migrate - with custom user models and custom manager models

--- bck_accnt/models.py
+++ bck_accnt/models.py
@@ -1,36 +1,9 @@
 from django.db import models
 
 from django.db import models
-from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
-import django.core.validators as validators
 from django.utils.translation import gettext as _
-
-class UserManager(BaseUserManager):
-    def create_user(self, email, password, nickname, **extra_fields):
-        if not email:
-            raise ValueError(_('The Email must be set'))
-        email = self.normalize_email(email)
-        nickname = models.CharField(
-            max_length=20,
-            unique=True,
-            validators=[
-            validators.RegexValidator(
-                r'^[\w.@+-]+$',_('Recommand using letters or numbers'))]
-        )
-        user = self.model(email=email, nickname=nickname, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-    def create_superuser(self, email, password, nickname, **extra_fields):
-        extra_fields.setdefault('is_staff',True)
-        extra_fields.setdefault('is_superuser',True)
-        extra_fields.setdefault('is_active',True)
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True'))
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True'))
-        return self.create_user(email, password, nickname, **extra_fields)
+from .model_manager import UserManager
 
 class User(AbstractUser):
     username = None
