f699e67ec3d1536a8351290a80f8667e8bb80b5e
diderot <dideroteffects@gmail.com>
2023-01-01 17:54:59 +0900

    temp3: plus secrets.json from settings.py key and database AND  API 사용 시 주의(해당 API(http://127.0.0.1:8000/dj-rest-auth/registration/) 에서는 username 값을 비워두고 사용 username을 입력하려면, name으로 요청

--- atc_web/settings.py
+++ atc_web/settings.py
@@ -1,10 +1,18 @@
 from pathlib import Path
+import os
+import json
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--f$u0_j#+km3-#n4d8q7li(1s+4o3f2raeo$co+9#*%h8qzf4('
+
+SECRET_BASE_FILE = os.path.join(BASE_DIR, 'secrets.json')
+secrets = json.loads(open(SECRET_BASE_FILE).read())
+for key, value in secrets.items():
+    setattr(sys.modules[__name__], key, value)
+# SECRET_KEY => secrets.json
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -15,26 +23,33 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    ###Base
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     ###bck
     'bck_accnt',
     'bck_stry',
     'django.contrib.sites',
-    
+    #allauth
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    
+    'allauth.socialaccount.providers.kakao',
+    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.google',
+    #DRF
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+    #dj-rest-auth
     'dj_rest_auth',
-    
     'dj_rest_auth.registration',
+    
     ###fnt
     'fntend',
 ]
@@ -51,24 +66,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'atc_web.urls'
 
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'fntend','static')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+
 
 WSGI_APPLICATION = 'atc_web.wsgi.application'
 
@@ -105,28 +105,16 @@ JWT_AUTH = {
     # 'JWT_EXPIRATION_DELTA' : datetime.timedelta(seconds=300),
 }
 
-REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER':'bck_accnt.serializers.UserSerializer'
-}
+# REST_AUTH_SERIALIZERS = {
+#     'USER_DETAILS_SERIALIZER':'bck_accnt.serializers.UserSerializer'
+# }
 
 REST_AUTH_REGISTER_SERIALIZERS = {
     'REGISTER_SERIALIZER':'bck_accnt.serializers.UserRegisterSerializer'
 }
 
 
-### Database
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'atc',
-        'USER': 'root',
-        'PASSWORD': 'ee2718ee',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
+### Database => screts.json
 
 ### Password validation
 
@@ -158,11 +146,29 @@ USE_I18N = True
 USE_TZ = True
 
 
+###html files
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(ROOT_DIR,'fntend','static')],#react
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 ### Static files (CSS, JavaScript, Images)
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'fntend','static'),
+    os.path.join(ROOT_DIR,'fntend','static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
