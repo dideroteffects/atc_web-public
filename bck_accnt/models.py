f861dd8022ae0df4fff3e297187319cf7fb2ceba
diderot <dideroteffects@gmail.com>
2023-01-01 13:38:17 +0900

    temp2: after migrate002

--- bck_accnt/models.py
+++ bck_accnt/models.py
@@ -7,8 +7,8 @@ class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True, blank=True, null=False)
     email = models.EmailField(_('email address'), unique=True)
     
-    # USERNAME_FIELD = 'nickname'
-    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
     
     objects = UserManager()
     
